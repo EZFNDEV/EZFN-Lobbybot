6e7b26c77de595a28fcad00cee05ccaa98fe3fcb
EZFN <56506141+LupusLeaks@users.noreply.github.com>
2020-10-16 18:52:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
+import os, sys
+sys.stderr = None
 print('\033[94mInstalling packages, please wait...')
 os.system('pip install -U EZFNSetup >/dev/null 2>&1')
 print('Installed the packages!\033[0m')
